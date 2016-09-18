8684fcdc79d04a296562b05403a1d86209ad489a
2183713z <18712799109@163.com>
2016-09-18 11:02:18 +0800

    data is what ?

--- src/About.js
+++ src/About.js
@@ -10,19 +10,26 @@ class About extends React.Component {
     }
   }
   componentDidMount(){
-    // console.log(searchGit);
     searchGit()
       .then( (data) => {
         console.log(data);
+        this.setState({
+          data:data.data,
+          wait:false
+        })
+        console.log(this.state.data);
       })
   }
   render () {
+    let gitInfo=(
+      <div>
+        <h3>{this.state.data.name}</h3>
+        <img src={this.state.data.avatar_url}/>
+      </div>
+    )
     return(
       <div>
-        {
-          this.state.wait ? '正在获取数据' :
-          <img src={this.state.data.avatar_url} />
-        }
+        {  this.state.wait ? '正在获取数据' : gitInfo }
       </div>
     )
   }
--- src/utils/helpers.js
+++ src/utils/helpers.js
@@ -3,7 +3,7 @@ import axios from 'axios';
 function searchGit(){
   return axios.get('https://api.github.com/users/2183713z')
     .then((res) => (
-    { data:res.data}
+    { data:res.data }
   ))
     .catch(function (error) {
       alert(error);
