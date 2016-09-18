934255a4e19828c54dce8686a12e0fe6b32a685d
2183713z <18712799109@163.com>
2016-09-18 14:48:04 +0800

    chuan json

--- src/utils/helpers.js
+++ src/utils/helpers.js
@@ -4,10 +4,21 @@ function searchGit(add){
   let address=`https://api.github.com/users/${add}`
   return axios.get(address)
     .then((res) => (
-    { data:res.data }
+    { getData:res.data }
   ))
     .catch(function (error) {
       alert(error);
     });
 }
 export {searchGit};
+function getJson(){
+  let address=`https://raw.githubusercontent.com/2183713z/9-18-aftern/master/card.json`
+  return axios.get(address)
+    .then( (res) =>(
+      {getJson:res.data}
+    ))
+    .catch(function (error){
+      alert(error)
+    })
+}
+export {getJson}
